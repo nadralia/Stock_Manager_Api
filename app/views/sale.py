e3e72d37a67a30a5b50cba5b9d42c1228d2cb240
nadralia <nadralia@gmail.com>
2018-10-19 18:07:39 +0300

    fixing the builds

--- app/views/sale.py
+++ app/views/sale.py
@@ -10,8 +10,8 @@ sales = list()
 
 
 @sale.route('/api/v1/sales', methods=['POST'])
-@swag_from('../api/v1/sales/add_sale.yml')
-def add_sale():
+@swag_from('../api/v1/sales/create_sale_record.yml')
+def create_sale_record():
     """ Creates a new sale record"""
     data = request.get_json()
     validate = Validate()
@@ -20,11 +20,11 @@ def add_sale():
         if valid == "Valid":
             record_id = len(sales)
             record_id += 1
-            total = int(data['price']) * int(data['prod_quantity'])
+            total = int(data['price']) * int(data['product_quantity'])
             date_added = datetime.now()
-            new_record = Sale(record_id, data['prod_name'],
+            new_record = Sale(record_id, data['product_name'],
                                     data['price'],
-                                    data['prod_quantity'],
+                                    data['product_quantity'],
                                     str(total), date_added)
             sales.append(new_record)
             return jsonify({"message": "record created successfully"}), 201
@@ -43,7 +43,7 @@ def fetch_sale_orders():
 
 @sale.route('/api/v1/sales/<int:sale_id>', methods=['GET'])
 @swag_from('../api/v1/sales/fetch_single_sale.yml')
-def fetch_single_record(sale_id):
+def get_single_record(sale_id):
     single_record = []
     if sale_id != 0 and sale_id <= len(sales):
         record = sales[sale_id - 1]
